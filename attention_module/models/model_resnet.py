9dcad28d3d8315e767ff0b19b19315473a1ff24e
Ogechi Onuoha <ogechi@staten.dcs.gla.ac.uk>
2021-02-07 13:15:32 +0000

    Experiment3-CBAM files

---
+++ attention_module/models/model_resnet.py
@@ -0,0 +1,282 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torch.nn import init
+#from torch.hub.utils import load_state_dict_from_url
+from .cbam import *
+from .bam import *
+
+"""def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)"""
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, use_cbam=False, groups=1,base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+        if use_cbam:
+            self.cbam = CBAM( planes, 16 )
+        else:
+            self.cbam = None
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        if not self.cbam is None:
+            out = self.cbam(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, use_cbam=False, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+        if use_cbam:
+            self.cbam = CBAM( planes * self.expansion, 16 )
+        else:
+            self.cbam = None
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        if not self.cbam is None:
+            out = self.cbam(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, network_type, num_classes, att_type=None, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None, norm_layer=None):
+
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+
+        self.groups = groups
+        self.base_width = width_per_group
+
+        self.network_type = network_type
+        # different model config between ImageNet and CIFAR 
+        if network_type == "ImageNet":
+            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+            
+            #self.avgpool = nn.AvgPool2d(7)
+            #self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        else:
+            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        if att_type=='BAM':
+            self.bam1 = BAM(64*block.expansion)
+            self.bam2 = BAM(128*block.expansion)
+            self.bam3 = BAM(256*block.expansion)
+        else:
+            self.bam1, self.bam2, self.bam3 = None, None, None
+
+        self.layer1 = self._make_layer(block, 64,  layers[0], att_type=att_type)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, att_type=att_type,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, att_type=att_type,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, att_type=att_type,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        nn.init.kaiming_normal_(self.fc.weight)
+        for key in self.state_dict():
+            if key.split('.')[-1]=="weight":
+                if "bn" in key:
+                    if "SpatialGate" in key:
+                        self.state_dict()[key][...] = 0
+            """elif key.split(".")[-1]=='bias':
+                self.state_dict()[key][...] = 0"""
+
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, att_type=None, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, att_type=='CBAM', self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+          layers.append(block(self.inplanes, planes, use_cbam=att_type=='CBAM', groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        if self.network_type == "ImageNet":
+            x = self.maxpool(x)
+
+        x = self.layer1(x)
+        if not self.bam1 is None:
+            x = self.bam1(x)
+
+        x = self.layer2(x)
+        if not self.bam2 is None:
+            x = self.bam2(x)
+
+        x = self.layer3(x)
+        if not self.bam3 is None:
+            x = self.bam3(x)
+
+        x = self.layer4(x)
+
+        if self.network_type == "ImageNet":
+            x = self.avgpool(x)
+        else:
+            x = F.avg_pool2d(x, 4)
+        #x = x.view(x.size(0), -1)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+def ResidualNet(network_type, depth, num_classes, att_type, pretrained=False, modelpath=None):
+
+    assert network_type in ["ImageNet", "CIFAR10", "CIFAR100"], "network type should be ImageNet or CIFAR10 / CIFAR100"
+    assert depth in [18, 34, 50, 101], 'network depth should be 18, 34, 50 or 101'
+
+    if depth == 18:
+        model = ResNet(BasicBlock, [2, 2, 2, 2], network_type, num_classes, att_type)
+        model_url='https://download.pytorch.org/models/resnet18-5c106cde.pth'
+
+    elif depth == 34:
+        model = ResNet(BasicBlock, [3, 4, 6, 3], network_type, num_classes, att_type)
+        model_url='https://download.pytorch.org/models/resnet34-333f7ec4.pth'
+
+    elif depth == 50:
+        model = ResNet(Bottleneck, [3, 4, 6, 3], network_type, num_classes, att_type)
+        model_url='https://download.pytorch.org/models/resnet50-19c8e357.pth'
+
+    elif depth == 101:
+        model = ResNet(Bottleneck, [3, 4, 23, 3], network_type, num_classes, att_type)
+        model_url='https://download.pytorch.org/models/resnet101-5d3b4d8f.pth'
+
+    if pretrained:
+        if not modelpath is None:
+            model_url = modelpath
+        state_dict = torch.hub.load_state_dict_from_url(model_url)
+        model.load_state_dict(state_dict)
+
+    return model
